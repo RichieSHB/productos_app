fe2e4e2a546e802ffb208e972ffdc7f4a9fde749
RichieSHB <83567168+RichieSHB@users.noreply.github.com>
2022-07-07 14:15:59 -0500

    Productos App

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/providers/login_form_provider.dart';
+import 'package:productos_app/ui/input_decorations.dart';
+import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 250,
+            ),
+            CardContainer(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Login',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ChangeNotifierProvider(
+                    create: (_) => LoginFormProvider(),
+                    child: const _LoginForm(),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            const Text(
+              'Create New Account',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 300,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
+
+    return Form(
+      key: loginForm.formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: Inputdecorations.authInputDecoration(
+                hintText: 'RajoPagu56@gmail.com',
+                labelText: 'Correo Electrónico',
+                prefixIcon: Icons.alternate_email_rounded),
+            onChanged: (value) => loginForm.email = value,    
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+              return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El correo no es valido';
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.multiline,
+            decoration: Inputdecorations.authInputDecoration(
+                hintText: '*************',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline_rounded),
+            onChanged: (value) => loginForm.password = value,    
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                  ? null
+                  : 'La contraseña debe de ser de al menos 6 caracteres';
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          MaterialButton(
+            onPressed: loginForm.isLoading ? null : () async {
+              FocusScope.of(context).unfocus();
+              if(!loginForm.isValidForm()) return;
+              loginForm.isLoading = true;
+              await Future.delayed(const Duration(seconds: 2));
+              loginForm.isLoading = false;
+              Navigator.pushReplacementNamed(context, 'home');
+            },
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            disabledColor: Colors.grey,
+            color: Colors.deepPurple,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+              child: Text(
+                loginForm.isLoading
+                ? 'Espere...'
+                : 'Ingresar',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
